b64ae1ea9af3be5d8dcd15757355c50f0523e714
Abdinajib <ynaj99@gmail.com>
2024-01-17 13:45:02 -0800

    created form and initialized states for the different inputs

--- RecipeApp/recipe-app/src/NewRecipes.js
+++ RecipeApp/recipe-app/src/NewRecipes.js
@@ -1,23 +1,56 @@
-import React from 'react'
+import { React, useState } from 'react';
+import './App.css';
 
 export default function NewRecipes() {
-  return (
-    <div style = {{padding: "20px",border: "2px solid black"}}>
-        <h3>New Recipe</h3>
-     <form>
-        <p>Recipe Name</p>
-        <input type="text"/>
-        <br/>
-        <p>Ingredients</p>
-        <textarea/>
-        <br/>
-        <p>Ingredients</p>
-        <textarea/>
-        <br/>
-        <button>RESET </button>
-        <br/>
-        <button>SAVE </button>
-     </form>
-    </div>
-  )
+    const [recName,setRecName] = useState("");
+    const [ingr, setIngr] = useState("");
+    const [dir, setDir] = useState("");
+     
+    return (
+        <div className = "container" style={{ padding: '20px', border: '1px solid #eee', width:"fit-content" }}>
+            <h3>New Recipe</h3>
+            <form style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start' }}>
+                <label htmlFor="recipeName">Recipe Name</label>
+                <br/>
+                <input type="text" id="recipeName" style={{ width: '412px'}} />
+                <p>Ingredients</p>
+                <textarea
+                    style={{ width: '412px', height: '132px' }}
+                />
+                <p>Directions</p>
+                <textarea
+                    style={{ width: '414px', height: '232px' }}
+
+                />
+                <br />
+                <div style = {{display:'flex', flexDirection:'column',alignItems:'center'}}>
+                <button
+                    style={{
+                        backgroundColor: '#1677ff',
+                        color: '#fff',
+                        padding: '10px 80px',
+                        border: 'none',
+                        borderRadius: '8px',
+                        // margin: '10px 0', 
+                    }}
+                    onClick = {saveHandler()}
+                >
+                    RESET
+                </button>
+                <br />
+                <button
+                    style={{
+                        backgroundColor: '#1677ff',
+                        color: '#fff',
+                        padding: '10px 80px',
+                        border: 'none',
+                        borderRadius: '8px',
+                    }}
+                >
+                    SAVE
+                </button>
+                </div>
+            </form>
+        </div>
+    );
 }
